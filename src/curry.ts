3514461a185487e23cb86cc411c8848bb6c06612
Eric Crosson <eric.s.crosson@utexas.edu>
2020-09-19 16:17:07 -0500

    Use simpler curry implementation
    One that doesn't wrap curried arguments in type-system arrays
    
    Closes #14

--- src/curry.ts
+++ src/curry.ts
@@ -1,36 +1,96 @@
 /**
- * Reference:
- * https://stackoverflow.com/questions/51859461/generic-curry-function-with-typescript-3/51860718#51860718
+ * Implementation sourced from @typed/curry:
+ * https://github.com/TylorS/typed-curry
  */
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-export function curry<F extends ((...args: any) => any)>(f: F): Curried<F> {
-    return function curried(...args: any[]): any {
-        return args.length < f.length
-            ? curried.bind(null, ...args)
-            : f.call(null, ...args as any)
-    }
+/* A function which takes exactly 1 parameter */
+export type Arity1<A, B> = (a: A) => B;
+
+/* A function which takes exactly 2 parameters */
+export type Arity2<A, B, C> = (a: A, b: B) => C;
+
+/* A function which takes exactly 3 parameters */
+export type Arity3<A, B, C, D> = (a: A, b: B, c: C) => D;
+
+/* A curried function of 0 to 1 parameters accepted */
+export interface Curry1<A, B> {
+    (): Curry1<A, B>;
+    (a: A): B;
 }
 
-type Length<T extends Array<any>> = T['length'];
+/* A curried function of 0 to 2 parameters accepted */
+export interface Curry2<A, B, C> {
+    (): Curry2<A, B, C>;
+    (a: A): Curry1<B, C>;
+    (a: A, b: B): C;
+}
 
-type Applies<F extends (...a: any[]) => any, L extends number> = [
-    never,
-    F extends ((a: infer A, ...z: infer Z) => infer R) ?
-        (a: A) => Length<Z> extends 0 ? R : (...z: Z) => R : never,
-    F extends ((a: infer A, b: infer B, ...z: infer Z) => infer R) ?
-        (a: A, b: B) => Length<Z> extends 0 ? R : (...z: Z) => R : never,
-    F extends ((a: infer A, b: infer B, c: infer C, ...z: infer Z) => infer R) ?
-        (a: A, b: B, c: C) => Length<Z> extends 0 ? R : (...z: Z) => R : never,
-    F extends ((a: infer A, b: infer B, c: infer C, d: infer D, ...z: infer Z) => infer R) ?
-        (a: A, b: B, c: C, d: D) => Length<Z> extends 0 ? R : (...z: Z) => R : never,
-    F extends ((a: infer A, b: infer B, c: infer C, d: infer D, e: infer E,...z: infer Z) => infer R) ?
-        (a: A, b: B, c: C, d: D, e: E) => Length<Z> extends 0 ? R : (...z: Z) => R : never
-][L];
+/* A curried function of 0 to 3 parameters accepted */
+export interface Curry3<A, B, C, D> {
+    (): Curry3<A, B, C, D>;
+    (a: A): Curry2<B, C, D>;
+    (a: A, b: B): Curry1<C, D>;
+    (a: A, b: B, c: C): D;
+}
 
-export type Curried<F extends ((...args: any[]) => any)> =
-    <L extends Parameters<Applies<F, number>>>(...args: L) =>
-    ReturnType<Applies<F, Length<L>>> extends Function
-    ? Curried<ReturnType<Applies<F, Length<L>>>>
-    : ReturnType<F>;
+function curry1<A, B>(fn: Arity1<A, B>): Curry1<A, B>;
+function curry1(fn: Arity1<any, any>): Curry1<any, any>;
+function curry1<A, B>(fn: Arity1<A, B>): Curry1<A, B> {
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    function curried (a: A) {
+        switch (arguments.length) {
+            case 0: return curried;
+            default: return fn(a);
+        }
+    }
+    return curried as Curry1<A, B>;
+};
+
+function curry2<A, B, C>(fern: Arity2<A, B, C>): Curry2<A, B, C>;
+function curry2(fn: Arity2<any, any, any>): Curry2<any, any, any>;
+function curry2<A, B, C>(fn: Arity2<A, B, C>): Curry2<A, B, C> {
+    function curried (a: A, b: B): any {
+        switch (arguments.length) {
+            case 0: return curried;
+            case 1: return curry1<B, C>((b: B) => fn(a, b));
+            default: return fn(a, b);
+        }
+    }
+    return curried as Curry2<A, B, C>;
+}
+
+function curry3<A, B, C, D>(fn: Arity3<A, B, C, D>): Curry3<A, B, C, D>;
+function curry3(fn: Arity3<any, any, any, any>): Curry3<any, any, any, any>;
+function curry3<A, B, C, D>(fn: Arity3<A, B, C, D>): Curry3<A, B, C, D> {
+    function curried (a: A, b: B, c: C): any {
+        switch (arguments.length) {
+            case 0: return curried;
+            case 1: return curry2<B, C, D>((b: B, c: C) => fn(a, b, c));
+            case 2: return curry1<C, D>((c: C) => fn(a, b, c));
+            default: return fn(a, b, c);
+        }
+    }
+    return curried as Curry3<A, B, C, D>;
+}
+
+export function curry<A, B>(fn: Arity1<A, B>): Curry1<A, B>;
+export function curry( fn: Arity1<any, any> ): Curry1<any, any>;
+
+export function curry<A, B, C>(fn: Arity2<A, B, C>): Curry2<A, B, C>;
+export function curry(fn: Arity2<any, any, any>): Curry2<any, any, any>;
+
+export function curry<A, B, C, D>(fn: Arity3<A, B, C, D>): Curry3<A, B, C, D>;
+export function curry(fn: Arity3<any, any, any, any>): Curry3<any, any, any, any>;
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function curry(fn: Function) {
+    switch (fn.length) {
+        case 0: return fn;
+        case 1: return curry1(fn as Arity1<any, any>);
+        case 2: return curry2(fn as Arity2<any, any, any>);
+        case 3: return curry3(fn as Arity3<any, any, any, any>);
+        default: throw new Error('This `curry` implementation does not support more than 3 arguments');
+    }
+}
---
+++ test/test-curry.ts
@@ -0,0 +1,41 @@
+import test from 'ava'
+
+/**
+ * Library under test
+ */
+
+import { curry } from '../src/curry'
+
+const id = <A>(a: A): A => a
+
+const fn = curry(
+    (_string: string, _number: number, _boolean: boolean): undefined => {
+        return undefined
+    }
+)
+
+test('should act as identity function when passed zero arguments', t => {
+    const curriedNone = fn()
+    const curriedOne = curriedNone('horse')
+    const curriedOneId = curriedOne()
+    const curriedTwo = curriedOneId(1)
+    const curriedTwoId = curriedTwo()
+    const curriedThree = curriedTwoId(true)
+    t.is(curriedThree, undefined)
+})
+
+test('should act as identity when passed function with zero arguments', t => {
+    const fn = curry(() => void 0)
+    t.is(fn(), undefined)
+})
+
+test('should curry unary function', t => {
+    const fn = curry(id)
+    const a = 5
+    t.is(a, fn(a))
+})
+
+test('should throw error when passed function with more than three arguments', t => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    t.throws(() => curry(((_a: number, _b: number, _c: number, _d: number) => void 0) as any))
+})
