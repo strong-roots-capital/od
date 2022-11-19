a99c712b119a3476073573bfbc876f385a6f07d0
Eric Crosson <eric.s.crosson@utexas.edu>
2022-11-19 11:29:20 -0600

    chore: add pre-commit.nix hooks

--- flake.nix
+++ flake.nix
@@ -1,16 +1,33 @@
 {
   description = "od";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    pre-commit-hooks,
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              alejandra.enable = true;
+            };
+          };
+        };
       in {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
@@ -18,6 +35,7 @@
           ];
           shellHook = ''
             export PATH="$PWD/node_modules/.bin:$PATH"
+            ${checks.pre-commit-check.shellHook}
           '';
         };
       }
